25ba8637b56fee1a54f714c7dc55f64cc3037a26
Z-ajamy <abdorahman0283@gmail.com>
2025-06-22 18:03:31 +0300

    div

--- executor.c
+++ executor.c
@@ -54,13 +54,14 @@ int executor(stack_t **top, command_t *command_ptr, unsigned int linenum)
     };
 
     /** Array of opcodes that do not require an argument */
-    instruction_t fun_no_arg[7] = {
+    instruction_t fun_no_arg[8] = {
         {"pall", pall},
         {"pint", pint},
         {"pop", pop},
         {"swap", swap},
         {"add", add},
         {"sub", sub},
+        {"div", _div},
         {NULL, NULL}
     };
 
